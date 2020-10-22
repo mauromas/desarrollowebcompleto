b0b3b391fd9319d0f7ee02217e9e292a66c3d869
mauromas <69467141+mauromas@users.noreply.github.com>
2020-10-22 20:29:27 -0300

    js
    js

---
+++ main.js
@@ -0,0 +1,213 @@
+(function() {
+    "use strict";
+
+    var regalo = document.getElementById('regalo');
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+
+
+        // Campos Datos Usuarios
+        var nombre = document.getElementById('nombre');
+        var apellido = document.getElementById('apellido');
+        var email = document.getElementById('email');
+
+
+        // Campos Pase
+        var pase_dia = document.getElementById('pase_dia');
+        var pase_dosdias = document.getElementById('pase_dosdias');
+        var pase_completo = document.getElementById('pase_completo');
+
+
+        //Botones y Divs
+        var calcular = document.getElementById('calcular');
+        var errorDiv = document.getElementById('error');
+        var botonRegistro = document.getElementById('btnRegistro');
+        var lista_productos = document.getElementById('lista-productos');
+        var suma = document.getElementById('suma-total');
+
+
+        //Extras 
+        var camisas = document.getElementById('camisa_evento');
+        var etiquetas = document.getElementById('etiquetas');
+
+
+
+        calcular.addEventListener('click', calcularMontos);
+
+        pase_dia.addEventListener('blur', mostrarDias);
+        pase_dosdias.addEventListener('blur', mostrarDias);
+        pase_completo.addEventListener('blur', mostrarDias);
+
+        nombre.addEventListener('blur', validarCampos);
+        apellido.addEventListener('blur', validarCampos);
+        email.addEventListener('blur', validarCampos);
+        email.addEventListener('blur', validarMail);
+
+
+        function validarCampos() {
+            if (this.value == '') {
+                errorDiv.style.display = 'block';
+                errorDiv.innerHTML = "Este campo es Obligatorio";
+                this.style.border = '1px solid red';
+                errorDiv.style.border = '1px solid red';
+            } else {
+                errorDiv.style.display = 'none';
+                this.style.border = '1px solid #cccccc';
+            }
+        }
+
+        function validarMail() {
+            if (this.value.indexOf("@") > -1) {
+                errorDiv.style.display = 'none';
+                this.style.border = '1px solid #cccccc';
+            } else {
+                errorDiv.style.display = 'block';
+                errorDiv.innerHTML = "Debe tener al menos un @";
+                this.style.border = '1px solid red';
+                errorDiv.style.border = '1px solid red';
+            }
+        }
+
+
+
+        function calcularMontos(event) {
+            event.preventDefault();
+            if (regalo.value === '') {
+                alert("Debes elegir un Regalo");
+                regalo.focus();
+            } else {
+                var boletosDia = parseInt(pase_dia.value, 10) || 0,
+                    boletosDosDias = parseInt(pase_dosdias.value, 10) || 0,
+                    boletosCompleto = parseInt(pase_completo.value, 10) || 0,
+                    cantCamisas = parseInt(camisas.value, 10) || 0,
+                    cantEtiquetas = parseInt(etiquetas.value, 10) || 0;
+
+                var totalPagar = (boletosDia * 30) + (boletosDosDias * 45) + (boletosCompleto * 50) + ((cantCamisas * 10) * .93) + (cantEtiquetas * 2);
+
+                var listadoProductos = [];
+
+                if (boletosDia >= 1) {
+                    listadoProductos.push(boletosDia + ' Pases por día');
+                }
+                if (boletosDosDias >= 1) {
+                    listadoProductos.push(boletosDosDias + ' Pases por 2 días');
+                }
+                if (boletosCompleto >= 1) {
+                    listadoProductos.push(boletosCompleto + ' Pases Completos');
+                }
+                if (cantCamisas >= 1) {
+                    listadoProductos.push(cantCamisas + ' Camisas')
+                }
+                if (cantEtiquetas >= 1) {
+                    listadoProductos.push(cantEtiquetas + ' Etiquetas')
+                }
+
+                lista_productos.style.display = "block";
+                lista_productos.innerHTML = '';
+                for (var i = 0; i < listadoProductos.length; i++) {
+                    lista_productos.innerHTML += listadoProductos[i] + '<br/>';
+                }
+
+                suma.innerHTML = "$ " + totalPagar.toFixed(2);
+
+            }
+        }
+
+        function mostrarDias() {
+            var boletosDia = parseInt(pase_dia.value, 10) || 0,
+                boletosDosDias = parseInt(pase_dosdias.value, 10) || 0,
+                boletosCompleto = parseInt(pase_completo.value, 10) || 0,
+
+                diasEligidos = [];
+
+            if (boletosDia > 0) {
+                diasEligidos.push('viernes');
+            }
+            if (boletosDosDias > 0) {
+                diasEligidos.push('viernes', 'sabado');
+            }
+            if (boletosCompleto > 0) {
+                diasEligidos.push('viernes', 'sabado', 'domingo');
+            }
+            for (var i = 0; i < diasEligidos.length; i++) {
+                document.getElementById(diasEligidos[i]).style.display = 'block';
+            }
+        }
+
+    }); // DOM CONTENT LOADED
+
+    var map = L.map('mapa').setView([-34.885438, -56.158032], 16);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([-34.885438, -56.158032]).addTo(map)
+        .bindPopup('Nuestro Local Center :D')
+        .openPopup();
+})();
+
+
+
+
+$(function() {
+
+    //Lettering
+    $('.nombre-sitio').lettering();
+
+    //Menu Fijo
+    var windowHeight = $(window).height();
+    var barraAltura = $('.barra').innerHeight();
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > windowHeight) {
+            $('.barra').addClass('fixed');
+            $('body').css({ 'margin-top': barraAltura + 'px' });
+        } else {
+            $('.barra').removeClass('fixed');
+            $('body').css({ 'margin-top': '0px' });
+        }
+    });
+
+    //Menu Responsive
+
+    $('.menu-movil').on('click', function() {
+        $('.navegacion-principal').slideToggle();
+    });
+
+
+
+    // Programa de Conferencia
+    $('div.ocultar').hide();
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+
+    $('.menu-programa a').on('click', function() {
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+        return false;
+
+    });
+
+
+    //Animaciones para los Numeros
+    $('.resumen-evento li:nth-child(1) p').animateNumber({ number: 6 }, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({ number: 15 }, 1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({ number: 3 }, 1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({ number: 9 }, 1200);
+
+
+    //Cuenta Regresiva
+    $('.cuenta-regresiva').countdown('2020/12/02 09:00:00', function(event) {
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
+
+});
